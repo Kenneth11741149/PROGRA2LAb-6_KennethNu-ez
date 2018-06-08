80be129f1940e9ea363f0eb761e208d6e990429e
Kenneth Nuñez <Kenneth11741149>
2018-06-08 13:21:12 -0600

    Creacion de clases

---
+++ LAb#6_KennethNuñez/src/lab/pkg6_kennethnuñez/Series.java
@@ -0,0 +1,20 @@
+
+package lab.pkg6_kennethnuñez;
+    
+import java.util.ArrayList;
+
+
+public class Series {
+    private String id;
+    private String nombre;
+    private int numtemporadas;
+    private String categoria;
+    private ArrayList<String> idiomas = new ArrayList();
+    private ArrayList<String> subtitulos = new ArrayList();
+    private String duracion;
+    private int rating;
+    private ArrayList<String> comentarios = new ArrayList();
+    private String productora;
+    private String director;
+    private ArrayList<String> actores = new ArrayList();
+}
